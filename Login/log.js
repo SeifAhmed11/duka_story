5d69ca0612edeb838ef76683fdc52796683c90c2
SeifAhmed11 <1seif1ahmed1@gmail.com>
2022-08-25 18:50:39 +0200

    11

--- Login/log.js
+++ Login/log.js
@@ -2,14 +2,15 @@ let back= document.getElementById("back");
 let box= document.getElementById("box");
 let back2= document.getElementById("back2");
 let box2= document.getElementById("box2");
-let sum = document.getElementById("sum");
-let ex = document.getElementById("ex");
-let headerbtn1 = document.getElementsByClassName("headerbtn1");
-let headerbtn = document.getElementById("headerbtn");
 let user="seif@gmail.com";
 let pass_user= "seif seif";
 
+
+
+
 document.getElementById("giftbtn").innerHTML = user[0]+user[1]+user[2]+user[3];
+
+
 
 function validateForm() {
     
@@ -28,7 +29,7 @@ function validateForm() {
     valid=false; }
     
     else if(document.inputForm.email.value=="Admin@admin.com" && document.inputForm.name.value=="11111111"){
-        location.href = "../index.html";
+        location.href = "/admin/index.html";
 valid=false; }
 else if(document.inputForm.email.value!=user || document.inputForm.name.value!=pass_user){
     back3.style.display = "block";
@@ -41,25 +42,18 @@ else if(document.inputForm.email.value!=user || document.inputForm.name.value!=p
     
     return valid;
 };
-let close = document.getElementById("close");
-
-close.onclick = function () {
+            
+function aa(){
     back.style.display = "none";
     box.style.display = "none";
-};
-
-let close2 = document.getElementById("close2");
-
-close2.onclick = function () {
+}
+function aa2(){
     back2.style.display = "none";
     box2.style.display = "none";
-};
-let close3 = document.getElementById("close3");
-
-close3.onclick = function () {
+}
+function aa3(){
     back3.style.display = "none";
     box3.style.display = "none";
-};
-
+}
 
 
